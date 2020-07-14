5fc0785d57cfb53ac4884398af6f5e22adbc9c5e
reedanna <reedanna@mail.com>
2020-07-14 17:58:19 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentences = ""
+    sentences = self.sub("?", ".")
+    sentences = sentences.sub("!", ".")
+    sentences = sentences.split(".")
+    sentences.count
   end
 end
